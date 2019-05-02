17d377ac2316a2f6c73595fdf7dc437747982124
travelranger <33505096+travelranger@users.noreply.github.com>
2019-05-03 01:55:39 +0800

    Add files via upload

---
+++ My Capstone practice 01.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # This Notebook is practce for Capstone Project with IBM Data Science Course
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone project Course!")
+
